b556e72b0b546f4ac8064907ba331e2f40913b3b
Emmanuel-Chevassus <56657301+Emmanuel-Chevassus@users.noreply.github.com>
2020-09-04 20:21:26 +0200

    Add files via upload

---
+++ Hurricane Dorian/main.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import metpy.calc as mpcalc
+from metpy.cbook import get_test_data
+from metpy.plots import add_metpy_logo, SkewT
+from metpy.units import units
+
+col_names = ['pressure', 'height', 'temperature', 'dewpoint', 'direction', 'speed']
+
+df = pd.read_fwf(get_test_data('may4_sounding.txt', as_file_obj=False),
+                 skiprows=5, usecols=[0, 1, 2, 3, 6, 7], names=col_names)
+
+# Drop any rows with all NaN values for T, Td, winds
+df = df.dropna(subset=('temperature', 'dewpoint', 'direction', 'speed'), how='all'
+               ).reset_index(drop=True)
+
+p = df['pressure'].values * units.hPa
+T = df['temperature'].values * units.degC
+Td = df['dewpoint'].values * units.degC
+wind_speed = df['speed'].values * units.knots
+wind_dir = df['direction'].values * units.degrees
+u, v = mpcalc.wind_components(wind_speed, wind_dir)
+
+fig = plt.figure(figsize=(9, 9))
+skew = SkewT(fig, rotation=45)
+
+# Plot the data using normal plotting functions, in this case using
+# log scaling in Y, as dictated by the typical meteorological plot.
+skew.plot(p, T, 'r')
+skew.plot(p, Td, 'g')
+skew.plot_barbs(p, u, v)
+skew.ax.set_ylim(1000, 100)
+skew.ax.set_xlim(-40, 60)
+
+# Calculate LCL height and plot as black dot. Because `p`'s first value is
+# ~1000 mb and its last value is ~250 mb, the `0` index is selected for
+# `p`, `T`, and `Td` to lift the parcel from the surface. If `p` was inverted,
+# i.e. start from low value, 250 mb, to a high value, 1000 mb, the `-1` index
+# should be selected.
+lcl_pressure, lcl_temperature = mpcalc.lcl(p[0], T[0], Td[0])
+skew.plot(lcl_pressure, lcl_temperature, 'ko', markerfacecolor='black')
+
+# Calculate full parcel profile and add to plot as black line
+prof = mpcalc.parcel_profile(p, T[0], Td[0]).to('degC')
+skew.plot(p, prof, 'k', linewidth=2)
+
+# Shade areas of CAPE and CIN
+skew.shade_cin(p, T, prof)
+skew.shade_cape(p, T, prof)
+
+# An example of a slanted line at constant T -- in this case the 0
+# isotherm
+skew.ax.axvline(0, color='c', linestyle='--', linewidth=2)
+
+
+# Add the relevant special lines
+skew.plot_dry_adiabats()
+skew.plot_moist_adiabats()
+skew.plot_mixing_lines()
+
+# Show the plot
+plt.show()
